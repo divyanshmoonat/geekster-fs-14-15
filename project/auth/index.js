9040b7b2c4aef44d0cfc083fba677f8888b7cf77
Divyansh <divyanshmoonat@outlook.com>
2024-06-04 22:04:17 +0530

    04-06-2024 | Payment Gateway Integration

--- project/auth/index.js
+++ project/auth/index.js
@@ -1,21 +1,40 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const { rateLimit } = require("express-rate-limit");
+const MongoStore = require("rate-limit-mongo");
+
+const passport = require("./middlewares/auth");
 
 const authRoutes = require("./routes/auth");
 const postRoutes = require("./routes/post");
 
-const authMiddleware = require("./middlewares/auth");
+// const authMiddleware = require("./middlewares/auth");
 
 const corsOptions = {
   origin: "http://localhost:5173",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
+const windowMs = 30 * 60 * 1000;
+
+const limiter = rateLimit({
+  store: new MongoStore({
+    uri: "mongodb://localhost:27017/authapp",
+    // should match windowMs
+    expireTimeMs: windowMs,
+    errorHandler: console.error.bind(null, "rate-limit-mongo"),
+    // see Configuration section for more options and details
+  }),
+  windowMs: windowMs, // 5 minutes
+  limit: 4, // Limit each IP to 25 requests per `window` (here, per 15 minutes).
+});
+
 const app = express();
 
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(limiter);
 
 mongoose
   .connect("mongodb://localhost:27017/authapp")
@@ -23,6 +42,10 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/posts", authMiddleware, postRoutes);
+app.use(
+  "/api/v1/posts",
+  passport.authenticate("jwt", { session: false }),
+  postRoutes
+);
 
 app.listen(8080, () => console.log("App is up and running at port 8080"));
--- project/auth/middlewares/auth.js
+++ project/auth/middlewares/auth.js
@@ -1,59 +1,85 @@
 const jwt = require("jsonwebtoken");
+const passport = require("passport");
+
+const JwtStrategy = require("passport-jwt").Strategy,
+  ExtractJwt = require("passport-jwt").ExtractJwt;
 
 const UserModel = require("../models/auth");
 
 const jwtSecretKey = "MY_JWT_SECRET_KEY123";
 
-const validateUser = async (req, res, next) => {
-  const headers = req.headers;
-  const tokenFromHeaders = headers.authorization.split(" ")[1];
-  /**
-   * Points to be validated in token
-   * 1. Token should be present
-   * 2. Secret key validation (This is the same tokne that we have generated)
-   * 3. Token expiry date should not be passed
-   * 4. Validate the issued at date (Optional)
-   * 5. Validate the user id if it is present in database
-   */
-  // 1
-  if (!tokenFromHeaders) {
-    return res.status(401).json({
-      msg: "Unauthenticated user",
-    });
-  }
+// const validateUser = async (req, res, next) => {
+//   const headers = req.headers;
+//   const tokenFromHeaders = headers.authorization.split(" ")[1];
+//   /**
+//    * Points to be validated in token
+//    * 1. Token should be present
+//    * 2. Secret key validation (This is the same tokne that we have generated)
+//    * 3. Token expiry date should not be passed
+//    * 4. Validate the issued at date (Optional)
+//    * 5. Validate the user id if it is present in database
+//    */
+//   // 1
+//   if (!tokenFromHeaders) {
+//     return res.status(401).json({
+//       msg: "Unauthenticated user",
+//     });
+//   }
 
-  // 2
-  try {
-    jwt.verify(tokenFromHeaders, jwtSecretKey);
-  } catch (err) {
-    return res.status(401).json({
-      msg: "Unauthenticated user",
-    });
-  }
+//   // 2
+//   try {
+//     jwt.verify(tokenFromHeaders, jwtSecretKey);
+//   } catch (err) {
+//     return res.status(401).json({
+//       msg: "Unauthenticated user",
+//     });
+//   }
 
-  // 3
-  const tokenData = jwt.decode(tokenFromHeaders);
-  // console.log(tokenData);
+//   // 3
+//   const tokenData = jwt.decode(tokenFromHeaders);
+//   // console.log(tokenData);
 
-  const tokenExp = tokenData.exp;
-  const now = Math.ceil(new Date().getTime() / 1_000);
+//   const tokenExp = tokenData.exp;
+//   const now = Math.ceil(new Date().getTime() / 1_000);
 
-  if (tokenExp < now) {
-    return res.status(401).json({
-      msg: "Unauthorized user",
-    });
-  }
+//   if (tokenExp < now) {
+//     return res.status(401).json({
+//       msg: "Unauthorized user",
+//     });
+//   }
 
-  // 5
-  const userId = tokenData.userId;
+//   // 5
+//   const userId = tokenData.userId;
+//   const user = await UserModel.findById(userId);
+//   if (!user) {
+//     return res.status(401).json({
+//       msg: "Unauthorized user",
+//     });
+//   }
+//   req.user = user; // This  will be passed to other available middlewares in next sequence and api routes
+//   next();
+// };
+
+// module.exports = validateUser;
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = jwtSecretKey;
+
+const strategy = new JwtStrategy(opts, async function (jwt_payload, done) {
+  const userId = jwt_payload.userId;
   const user = await UserModel.findById(userId);
   if (!user) {
-    return res.status(401).json({
-      msg: "Unauthorized user",
-    });
+    return done("Invalid user", false);
   }
-  req.user = user; // This  will be passed to other available middlewares in next sequence and api routes
-  next();
-};
+  if (user) {
+    return done(null, user);
+  } else {
+    return done(null, false);
+    // or you could create a new account
+  }
+});
 
-module.exports = validateUser;
+passport.use(strategy);
+
+module.exports = passport;
